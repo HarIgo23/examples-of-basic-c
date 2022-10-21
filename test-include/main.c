29e9d353b6ee0fb17322358fdfd3b3247534770e
HarIgo23 <igorn2304@gmail.com>
2022-10-22 00:52:08 +0300

    Add tests for include directive

---
+++ test-include/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "num.c"
+
+int main(void)
+{
+    printf("%d\n", number);
+    return 0;
+}
---
+++ test-include/mainalt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "num.h"
+#include "num.c"
+
+int main(void)
+{
+    printf("%d\n", number);
+    return 0;
+}
