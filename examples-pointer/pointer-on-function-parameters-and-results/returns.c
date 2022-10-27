91c9780a23f4d617d0e67ac3b7fc85039d4b7c8c
HarIgo23 <igorn2304@gmail.com>
2022-10-27 13:35:36 +0300

    Add examples for pointer on function in return

---
+++ examples-pointer/pointer-on-function-parameters-and-results/returns.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int action1(void)
+{
+    printf("Action 1");
+    return 1;
+}
+
+int action2(void)
+{
+    printf("Action 2");
+    return 2;
+}
+
+int action3(void)
+{
+    printf("Action 3");
+    return 3;
+}
+
+
+int (*select(void))(void)
+{
+    int choice;
+
+    int (*actions[])() = {action1, action2, action3};
+    printf("Select action (1, 2, 3): ");
+    scanf("%d", &choice);
+
+    if (choice > 0 && choice < 4)
+        return actions[choice-1];
+    else
+        return NULL;
+}
+
+
+int main(void)
+{
+    int (*action)(void); // указатель на выбранную функцию
+    int actionNumber; // результат функции - номер действия
+
+    while(1)
+    {
+        action = select(); // получаем указатель на функцию
+        if(action==NULL)
+            break;
+        actionNumber = action(); // выполняем функцию
+        printf("\nselected action %d \n", actionNumber);
+    }
+
+    printf("End\n");
+    return 0;
+}
