a42383aeecf548119f99890c04e958a668c87689
HarIgo23 <igorn2304@gmail.com>
2022-11-20 11:10:51 +0300

    Add third exercise of sixth chapter in Pratas book

---
+++ stephen-prata-book/sixth-chapter/third.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define START_CHAR 'F'
+
+int main(void)
+{
+    for (int i = 1; i <= 6; i++)
+    {
+        for(int j = 0; j < i; j++)
+            printf("%c", START_CHAR - j);
+        printf("\n");
+    }
+    return 0;
+}
